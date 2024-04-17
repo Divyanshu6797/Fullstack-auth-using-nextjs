3b0865382d80d28a29e3f0563b48d34ad64d5693
Divyanshu6797 <divyanshu6797@gmail.com>
2024-04-17 11:20:22 +0530

    signup and login added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,42 @@
 'use client'
+import axios from "axios"
 import { useState } from "react"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
 
 const LoginPage = () => {
+  const router = useRouter()
+  const [loading,setLoading] = useState(false)
 
   const [user,setUser] = useState({
     
     email : "",
     password : ""
 })
-const login = async () => {
+const onLogin = async () => {
+
+  try {
+    setLoading(true)
+    const response = await axios.post("/api/users/login", user);
+
+    console.log("login successfull");
+    toast.success("login successfull");
+    router.push("/profile");
+    
+    
+  } catch (error:any) {
+    console.log("login failed")
+    console.log(error)
+    console.log(error.message)
+    
+  } finally {
+    setLoading(false)
+  }
 
 }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">{loading ? "Processing" : "Login"}</h1>
         <form className="w-full max-w-sm">
   
   <div className="md:flex md:items-center mb-6">
@@ -39,7 +63,7 @@ const login = async () => {
   <div className="md:flex md:items-center">
     <div className="md:w-1/3"></div>
     <div className="md:w-2/3">
-      <button onClick={login} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+      <button onClick={onLogin} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
         Log in 
       </button>
     </div>
